1fd680b041d6bec53e371b1a6aea449b5a729508
oladee <deemajor230600@gmail.com>
2024-07-08 18:02:59 +0100

    dialect module

--- config/sequel.js
+++ config/sequel.js
@@ -1,7 +1,10 @@
 const Sequelize = require('sequelize')
+import pg from 'pg';
 require('dotenv').config()
 
-const sequelize = new Sequelize(process.env.SEQUEL)
+const sequelize = new Sequelize(process.env.SEQUEL,{
+    dialectModule: pg
+  })
 
 
 module.exports = sequelize
